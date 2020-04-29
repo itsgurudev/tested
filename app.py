88d381879feffd321e0ceaab7c66225f2475fe42
Mr. Maity <smtt94@gmail.com>
2020-04-29 18:33:07 +0530

    before session v0.2

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request#, session
+#from flask_session import Session
 
 app = Flask(__name__)
 
+#app.config["SESSION_PERMANENT"]=False
+#app.config["SESSION_TYPE"]="filesystem"
+#Session(app)
+
+notelist=[]
+
+
 @app.route('/')
-def helloWorld():
-    ok= "fuck you!"
-    return render_template("index.html", OK=ok)
+def index():
+    names=["ok", "fine", "stop", "this"]
+    return render_template("index.html", names=names)
+
+@app.route('/bye')
+def bye():
+    names=["don't", "do", "this", "to", "me", "fuck", "this", "shit", 56]
+    return render_template("index.html", names=names)
+
+
+@app.route('/calc')
+def calc():
+    return render_template("calc.html")
+
+@app.route('/result', methods=["GET", "POST"])
+def result():
+    result=0
+    num=0
+    if request.method == "POST":
+        num=request.form.get("num")
+        if num=="":
+            num=0
+        num=float(num)
+        result= num*num
+        
+    return render_template("result.html",num=num, result=result)
+   
+@app.route('/notes', methods=["GET", "POST"])
+def notes():   
+    if request.method == "POST":
+        note=request.form.get("note")
+        notelist.append(note)
+        
+    return render_template("notes.html",notelist=notelist)
